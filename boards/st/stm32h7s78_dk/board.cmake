c8cd136fb04cd9d81cfc1919cd73ad3af7e137df
Francois Ramu <francois.ramu@st.com>
2024-05-27 11:18:52 +0200

    boards: arm: stm32h7s78 disco kit supporting xspi instance
    Add the XSPI 2 which is an octoSPI connection to a octo NOR flash
    mx66uw1g45 on the stm32h7s78_dk disco kit
    Use the STM32Cube programmer to flash with the corresponding
    external loader for XiP mode.
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- boards/st/stm32h7s78_dk/board.cmake
+++ boards/st/stm32h7s78_dk/board.cmake
@@ -3,6 +3,14 @@
 board_runner_args(stm32cubeprogrammer "--erase" "--port=swd" "--reset-mode=hw")
 board_runner_args(openocd --target-handle=_CHIPNAME.cpu0)
 
+if(CONFIG_STM32_MEMMAP)
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+board_runner_args(stm32cubeprogrammer "--hex-file=${ZEPHYR_BASE}/build/zephyr/zephyr.hex")
+board_runner_args(stm32cubeprogrammer "--extload=MX66UW1G45G_STM32H7S78-DK.stldr")
+else()
+board_runner_args(stm32cubeprogrammer "--erase" "--port=swd" "--reset-mode=hw" )
+endif()
+
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 # FIXME: openocd runner not yet available.
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
